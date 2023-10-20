00ded879ccb317df42cdf083b465639d2a771135
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2023-10-20 14:29:24 -0300

    feat: finalizado parte inicial da tela de descricao do produto

--- src/screens/ProductDetail/index.tsx
+++ src/screens/ProductDetail/index.tsx
@@ -1,22 +1,51 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import * as S from './styles';
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {ProductsStackParamList} from '../../routes/ProductsRoutes';
-import Icon from 'react-native-vector-icons/AntDesign';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ProductService from '../../services/ProductService';
 import {IProduct} from '../../interfaces/IProduct';
 import {Rating} from '../../components/Rating';
+import {formatNumberToCurrency} from '../../utils/formatNumberToCurrency';
+import {Button} from '../../components/Button';
+import {useCart} from '../../contexts/CartProvider';
+import {useTheme} from 'styled-components/native';
 
 export function ProductDetail() {
   const navigation = useNavigation();
+
   const route = useRoute<RouteProp<ProductsStackParamList, 'ProductDetail'>>();
   const {id} = route.params;
 
+  const {cartProducts, incrementProductInCart, decrementProductInCart} =
+    useCart();
+  const theme = useTheme();
+
   const [product, setProduct] = useState<IProduct | null>(null);
+
+  const productCartQuantity =
+    cartProducts.find(cartProduct => cartProduct.id === product?.id)
+      ?.quantity ?? 0;
 
   const goBack = () => {
     navigation.goBack();
   };
+
+  const addToCart = useCallback(() => {
+    incrementProductInCart(product!);
+  }, [product, incrementProductInCart]);
+
+  const removeFromCart = useCallback(() => {
+    decrementProductInCart(product!.id);
+  }, [product, decrementProductInCart]);
+
+  const onFinish = useCallback(() => {
+    if (productCartQuantity > 0) {
+      addToCart();
+    }
+  }, [addToCart, productCartQuantity]);
 
   useEffect(() => {
     if (id) {
@@ -30,15 +59,94 @@ export function ProductDetail() {
 
   return (
     <S.ProductDetailContainer>
-      <S.BackButton onPress={goBack}>
-        <Icon name="arrowleft" size={24} color="#313131" />
-      </S.BackButton>
+      <S.Header>
+        <Button
+          onPress={goBack}
+          variant="tertiary"
+          icon={
+            <AntDesign name="arrowleft" size={24} color={theme['text-light']} />
+          }
+        />
+      </S.Header>
       {product && (
         <>
-          <S.CategoryRateContainer>
-            <S.Category>{product.category}</S.Category>
-            <Rating count={product.rating.count} rate={product.rating.rate} />
-          </S.CategoryRateContainer>
+          <S.ProductInfoScroll>
+            <S.ProductInfoContainer>
+              <S.CategoryRateContainer>
+                <S.Category>{product.category}</S.Category>
+                <Rating
+                  count={product.rating.count}
+                  rate={product.rating.rate}
+                />
+              </S.CategoryRateContainer>
+              <S.Title>{product.title}</S.Title>
+              <S.Image
+                source={{
+                  uri: product.image,
+                }}
+              />
+              <S.Value>{formatNumberToCurrency(product.price)}</S.Value>
+              <S.Separator />
+              <S.Description>{product.description}</S.Description>
+            </S.ProductInfoContainer>
+          </S.ProductInfoScroll>
+          <S.ActionsContainer>
+            {productCartQuantity > 0 ? (
+              <S.AddSubtractContainer>
+                <Button
+                  onPress={removeFromCart}
+                  icon={
+                    productCartQuantity === 1 ? (
+                      <FontAwesome
+                        name="trash"
+                        color={theme['primary-dark']}
+                        size={24}
+                      />
+                    ) : (
+                      <AntDesign
+                        name="minus"
+                        color={theme['primary-dark']}
+                        size={24}
+                      />
+                    )
+                  }
+                  variant="secondary"
+                  size="large"
+                />
+                <S.CountContainer>
+                  <Ionicons
+                    name="cart"
+                    color={theme['primary-dark']}
+                    size={28}
+                  />
+                  <S.CountLabel>{productCartQuantity}</S.CountLabel>
+                </S.CountContainer>
+                <Button
+                  onPress={addToCart}
+                  icon={
+                    <AntDesign
+                      name="plus"
+                      color={theme['primary-dark']}
+                      size={24}
+                    />
+                  }
+                  variant="secondary"
+                  size="large"
+                />
+              </S.AddSubtractContainer>
+            ) : (
+              <Button
+                onPress={addToCart}
+                label="Adicionar no carrinho"
+                variant="secondary"
+              />
+            )}
+            <Button
+              onPress={onFinish}
+              label={productCartQuantity > 0 ? 'Finalizar compra' : 'Comprar'}
+              size="large"
+            />
+          </S.ActionsContainer>
         </>
       )}
     </S.ProductDetailContainer>
--- src/screens/ProductDetail/styles.ts
+++ src/screens/ProductDetail/styles.ts
@@ -1,18 +1,17 @@
 import styled from 'styled-components/native';
 
+import {Dimensions} from 'react-native';
+
+const {width} = Dimensions.get('screen');
+
+export const Header = styled.View`
+  align-items: flex-start;
+`;
+
 export const ProductDetailContainer = styled.View`
   flex: 1;
   padding: 16px;
   background-color: ${props => props.theme.background};
-`;
-
-export const BackButton = styled.TouchableOpacity`
-  width: 40px;
-  height: 40px;
-  border-radius: 10px;
-  border: solid 1px ${props => props.theme.border};
-  align-items: center;
-  justify-content: center;
 `;
 
 export const CategoryRateContainer = styled.View`
@@ -25,4 +24,63 @@ export const Category = styled.Text`
   font-size: 14px;
 `;
 
-export const Detail = styled.Text``;
+export const Title = styled.Text`
+  color: ${props => props.theme.text};
+  font-size: 25px;
+  font-weight: bold;
+`;
+
+export const Image = styled.Image`
+  width: ${width * 0.8}px;
+  height: ${width * 0.8}px;
+  object-fit: contain;
+  align-self: center;
+`;
+
+export const Value = styled.Text`
+  color: ${props => props.theme.text};
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const Separator = styled.View`
+  width: ${width - 32}px;
+  height: 1px;
+  background-color: ${props => props.theme.border};
+`;
+
+export const Description = styled.Text`
+  color: ${props => props.theme.text};
+  font-size: 14px;
+`;
+
+export const ProductInfoContainer = styled.View`
+  flex: 1;
+`;
+
+export const ProductInfoScroll = styled.ScrollView`
+  flex: 1;
+`;
+
+export const ActionsContainer = styled.View`
+  gap: 8px;
+  padding-top: 8px;
+`;
+
+export const AddSubtractContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const CountContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const CountLabel = styled.Text`
+  color: ${props => props.theme.text};
+  font-size: 32px;
+  font-weight: bold;
+`;
