dbe0b8b550a5fe555805e0dc747d210981526b68
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2023-10-21 00:20:42 -0300

    feat: adicionando icones no tab menu

--- src/routes/BottomTabRoutes.tsx
+++ src/routes/BottomTabRoutes.tsx
@@ -3,6 +3,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ProductsRoutes} from './ProductsRoutes';
 import {FavoritesRoutes} from './FavoritesRoutes';
 import {CartRoutes} from './CartRoutes';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useTheme} from 'styled-components/native';
 
 export type BottomTabParamList = {
   ProductsTab: undefined;
@@ -13,14 +16,19 @@ export type BottomTabParamList = {
 const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 export function BottomTabRoutes() {
+  const theme = useTheme();
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: theme.primary,
       }}>
       <Tab.Screen
         options={{
           title: 'Produtos',
+          tabBarIcon: ({color, size}) => (
+            <AntDesign name="CodeSandbox" color={color} size={size} />
+          ),
         }}
         name="ProductsTab"
         component={ProductsRoutes}
@@ -28,6 +36,9 @@ export function BottomTabRoutes() {
       <Tab.Screen
         options={{
           title: 'Favoritos',
+          tabBarIcon: ({color, size}) => (
+            <AntDesign name="heart" color={color} size={size} />
+          ),
         }}
         name="FavoritesTab"
         component={FavoritesRoutes}
@@ -35,6 +46,9 @@ export function BottomTabRoutes() {
       <Tab.Screen
         options={{
           title: 'Carrinho',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="cart" color={color} size={size} />
+          ),
         }}
         name="CartTab"
         component={CartRoutes}
--- src/routes/ProductsRoutes.tsx
+++ src/routes/ProductsRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Products} from '../screens/ProductsList';
 import {ProductDetail} from '../screens/ProductDetail';
+import {useTheme} from 'styled-components/native';
 
 export type ProductsStackParamList = {
   Products: undefined;
@@ -13,10 +14,14 @@ export type ProductsStackParamList = {
 const Stack = createNativeStackNavigator<ProductsStackParamList>();
 
 export function ProductsRoutes() {
+  const theme = useTheme();
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
+        contentStyle: {
+          backgroundColor: theme.background,
+        },
       }}>
       <Stack.Screen name="Products" component={Products} />
       <Stack.Screen name="ProductDetail" component={ProductDetail} />
