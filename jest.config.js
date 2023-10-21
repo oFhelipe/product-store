2aa8b70327fb3abae506548348eb0c4955784fc7
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2023-10-21 20:17:04 -0300

    test: criando arquivo de setup para o jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+    '@testing-library/jest-native/extend-expect',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
   ],
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-native/extend-expect';
+import {jest} from '@jest/globals';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
