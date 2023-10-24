3876033e6b6463614fce7ab985b2ad0660bb2e90
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2023-10-24 17:53:28 -0300

    chore: adicionando splash screen e icone

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Routes} from './src/routes';
 import styled, {ThemeProvider} from 'styled-components/native';
 import {defaultTheme} from './src/styles/themes/default';
 import {FavoritesProvider} from './src/contexts/FavoritesProvider';
 import {CartProvider} from './src/contexts/CartProvider';
+import SplashScreen from 'react-native-splash-screen';
 
 const SafeAreaViewContainer = styled.SafeAreaView`
   flex: 1;
 `;
 
 export function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <FavoritesProvider>
